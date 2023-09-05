d6d85f96d7a8d65c89c6ce5d74a03f0cf3ef0589
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-09-05 08:34:24 +0800

    Create string-nchar-toupper-tolower-substring-strsplit.R

---
+++ RUNOOB/string-nchar-toupper-tolower-substring-strsplit.R
@@ -0,0 +1,15 @@
+result <- nchar("Google Runoob Taobao")
+print(result)
+
+result <- toupper("Runoob")
+print(result)
+
+result <- tolower("Runoob")
+print(result)
+
+result <- substring("Runoob",2,5)
+print(result)
+
+str <- "Hello,World!"
+split_str <- strsplit(str,",")
+print(split_str)
