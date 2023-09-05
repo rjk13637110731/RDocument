b97e4ce4247e8e1bc8f73ad1ec1c5e8a8851bc85
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-09-05 08:45:17 +0800

    Create list.R

---
+++ RUNOOB/list.R
@@ -0,0 +1,36 @@
+list_data <- list("runoob","google",c(11,22,33),123,51.23,119.1)
+print(list_data)
+
+numbers <- c(1,2,3,4,5)
+characters <- c("apple","banana","orange")
+merged_vector <- c(numbers,c(6,7,8))
+merged_charcters <- c(characters,c("grape","melon"))
+print(merged_vector)
+print(merged_charcters)
+
+
+list_data <- list(c("google","Runoob","Taobao"),matrix(c(1,2,3,4,5,6),nrow=2),list("Runoob",12.3))
+names(list_data) <- c("Sites","Numbers","Lists")
+print(list_data)
+
+
+list_data <- list(c("google","Runoob","Taobao"),matrix(c(1,2,3,4,5,6),nrow=2),list("Runoob",12.3))
+names(list_data) <- c("Sites","Numbers","Lists")
+print(list_data[1])
+print(list_data[3])
+print(list_data$Numbers)
+
+
+list_data <- list(c("google","Runoob","Taobao"),matrix(c(1,2,3,4,5,6),nrow=2),list("Runoob",12.3))
+names(list_data) <- c("Sites","Numbers","Lists")
+list_data[4] <- "新元素"
+print(list_data[4])
+list_data[4] <- NULL
+print(list_data[4])
+list_data[3] <- "我来替换第三个元素"
+print(list_data[3])
+
+my_list <- list(1,2,3,"a","b","c")
+for (element in my_list){
+  print(element)
+}
