40b65c84b7bb9d1453d126fffc983ec82e22c7df
TangJ <86515447+rjk13637110731@users.noreply.github.com>
2023-09-05 08:24:48 +0800

    Create string.R

---
+++ RUNOOB/string.R
@@ -0,0 +1,7 @@
+a <- "Google"
+b <- "Runoob"
+c <- "Taobao"
+print(paste(a,b,c))
+print(paste(a,b,c,sep="-"))
+print(paste(letters[1:6],1:6,sep="",collapse="="))
+print(paste(letters[1:6],1:6,collapse="."))
